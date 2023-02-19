b5282e7078913b7d80ca6c93015e37661e02558f
kazimohsin <124923791+kazimohsin@users.noreply.github.com>
2023-02-19 10:54:45 -0500

    Add files via upload

---
+++ Assignmrnt_que_01.py
@@ -0,0 +1,7 @@
+l1 = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+def last(l): return l[-1]
+
+def sort_list_last(tuples):
+  return sorted(tuples, key=last)
+
+print(sort_list_last([(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]))
